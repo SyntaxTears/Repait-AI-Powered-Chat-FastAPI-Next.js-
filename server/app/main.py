82334e43b653c3f5468f64efeb1690a2affaa92c
SyntaxTears <vitaliimakohin@gmail.com>
2025-05-04 23:43:55 +0300

    FIX CORS

--- server/app/main.py
+++ server/app/main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="Detect Auto API")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["*"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
